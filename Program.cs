01298518b023431cfff0e6fde4b4a1e9abc31d00
shauniif <francescavescio12@gmail.com>
2024-06-21 09:51:23 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROGETTO_SETTIMINALE_BE_S1_L5__Vescio_Pia_Francesca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
